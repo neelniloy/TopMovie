e0fcfe80469dd58791177e1d8f34983218fc3b60
neelniloy <niloy64529@gmail.com>
2023-02-06 18:17:08 +0600

    add drawer

--- app/src/main/java/com/bdjobsniloy/movieapp/MainActivity.kt
+++ app/src/main/java/com/bdjobsniloy/movieapp/MainActivity.kt
@@ -3,23 +3,34 @@ package com.bdjobsniloy.movieapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import com.bdjobsniloy.movieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
         window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
-        setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
 
-
+    }
+    fun openCloseNavigationDrawer(view: View) {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
     }
 }
