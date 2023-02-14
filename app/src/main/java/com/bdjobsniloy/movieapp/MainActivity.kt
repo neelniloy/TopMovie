0106ce3354e875af40de0f45ecce27e9b061bd51
Niloy Sarker <niloy64529@gmail.com>
2023-02-14 15:21:07 +0600

    Added Internet Connection Checker

--- app/src/main/java/com/bdjobsniloy/movieapp/MainActivity.kt
+++ app/src/main/java/com/bdjobsniloy/movieapp/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.bdjobsniloy.movieapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.core.view.GravityCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.bdjobsniloy.movieapp.databinding.ActivityMainBinding
+import org.imaginativeworld.oopsnointernet.callbacks.ConnectionCallback
+import org.imaginativeworld.oopsnointernet.dialogs.signal.NoInternetDialogSignal
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -56,6 +55,38 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+
+        // No Internet Dialog
+        NoInternetDialogSignal.Builder(
+            this,
+            lifecycle
+        ).apply {
+            dialogProperties.apply {
+                connectionCallback = object : ConnectionCallback { // Optional
+                    override fun hasActiveConnection(hasActiveConnection: Boolean) {
+
+
+                    }
+                }
+                cancelable = false // Optional
+                noInternetConnectionTitle = "No Internet" // Optional
+                noInternetConnectionMessage =
+                    "Check your Internet connection and try again." // Optional
+                showInternetOnButtons = true // Optional
+                pleaseTurnOnText = "Please turn on" // Optional
+                wifiOnButtonText = "Wifi" // Optional
+                mobileDataOnButtonText = "Mobile data" // Optional
+
+                onAirplaneModeTitle = "No Internet" // Optional
+                onAirplaneModeMessage = "You have turned on the airplane mode." // Optional
+                pleaseTurnOffText = "Please turn off" // Optional
+                airplaneModeOffButtonText = "Airplane mode" // Optional
+                showAirplaneModeOffButtons = true // Optional
+            }
+        }.build()
+
+
     }
 
     fun openCloseNavigationDrawer() {
@@ -65,4 +96,5 @@ class MainActivity : AppCompatActivity() {
             binding.drawerLayout.openDrawer(binding.navView)
         }
     }
+
 }
